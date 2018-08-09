1860c5de4586e65841af76d9a389d3670c323318
Valentin <valentin@bailfacile.fr>
2018-08-09 15:57:29 +0200

    Change package config folder name

--- src/ChargebeeServiceProvider.php
+++ src/ChargebeeServiceProvider.php
@@ -16,7 +16,7 @@ class ChargebeeServiceProvider extends ServiceProvider
     {
         // Copies the config file to project config directory on: php artisan vendor:publish
         $this->publishes([
-            __DIR__.'/Config/chargebee.php' => config_path('chargebee.php'),
+            __DIR__.'/config/chargebee.php' => config_path('chargebee.php'),
         ], 'config');
 
         // Publishes the migrations into the application's migrations folder
